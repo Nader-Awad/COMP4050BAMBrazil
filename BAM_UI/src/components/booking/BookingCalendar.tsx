7c043d1c332817005b572f73e2f9eba4d73513ef
Nader Awad <awadnader64@gmail.com>
2025-09-08 07:11:46 +1000

    removing unused React import

--- BAM_UI/src/components/booking/BookingCalendar.tsx
+++ BAM_UI/src/components/booking/BookingCalendar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { CalendarClock, Clock } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
